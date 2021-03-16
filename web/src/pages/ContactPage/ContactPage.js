10c3f3210ca524b8d8fd7fd7e9ae61b8ea3fbe9d
Richard Powell <richardo@gmail.com>
2021-03-16 13:53:14 -0400

    Add validation to the contact form

--- web/src/pages/ContactPage/ContactPage.js
+++ web/src/pages/ContactPage/ContactPage.js
@@ -5,17 +5,45 @@ import {
   Submit,
   FieldError,
   Label,
+  FormError,
 } from '@redwoodjs/forms'
+import { useForm } from 'react-hook-form'
+import { Flash, useFlash, useMutation } from '@redwoodjs/web'
 import BlogLayout from 'src/layouts/BlogLayout'
 
+const CREATE_CONTACT = gql`
+  mutation CreateContactMutation($input: CreateContactInput!) {
+    createContact(input: $input) {
+      id
+    }
+  }
+`
+
 const ContactPage = () => {
+  const { addMessage } = useFlash()
+  const formMethods = useForm({ mode: 'onBlur' })
+
+  const [create, { loading, error }] = useMutation(CREATE_CONTACT, {
+    onCompleted: () => {
+      addMessage('Thank you for your submission!', {
+        style: { backgroundColor: 'green', color: 'white', padding: '1rem' },
+      })
+      formMethods.reset()
+    },
+  })
+
   const onSubmit = (data) => {
-    console.log(data)
+    create({ variables: { input: data } })
   }
 
   return (
     <BlogLayout>
-      <Form onSubmit={onSubmit} validation={{ mode: 'onBlur' }}>
+      <Flash timeout={2000} />
+      <Form onSubmit={onSubmit} error={error} formMethods={formMethods}>
+        <FormError
+          error={error}
+          wrapperStyle={{ color: 'red', backgroundColor: 'lavenderblush' }}
+        />
         <Label name="name" errorClassName="error">
           Name
         </Label>
@@ -28,20 +56,16 @@ const ContactPage = () => {
         />
         <FieldError name="name" className="error" />
 
-        <Label name="email" errorClassName="error">
-          Email
-        </Label>
-        <TextField
+        <Label
           name="email"
+          errorClassName="error"
           validation={{
             required: true,
-            pattern: {
-              value: /[^@]+@[^.]+\..+/,
-            },
-            message: 'Please enter a valid email address',
           }}
-          errorClassName="error"
-        />
+        >
+          Email
+        </Label>
+        <TextField name="email" errorClassName="error" />
         <FieldError name="email" className="error" />
 
         <Label name="message" errorClassName="error">
@@ -54,7 +78,7 @@ const ContactPage = () => {
         />
         <FieldError name="message" className="error" />
 
-        <Submit>Save</Submit>
+        <Submit disabled={loading}>Save</Submit>
       </Form>
     </BlogLayout>
   )
